bf551b5bf957f46ddd12420b5f4070db916c95bb
Joseph Guerrero <joseph.guerrero1@outlook.com>
2021-04-20 15:48:28 -0600

    Finished variables

---
+++ src/components/tweetPage.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+      <p></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "tweet-page",
+  data: function () {
+    return {
+        tweets = [
+        tweetOne = {
+            user: "Joseph",
+            content: "Must go faster... go, go, go, go, go! Checkmate... We gotta burn the rain forest, dump toxic waste, pollute the air, and rip up the OZONE! 'Cause maybe if we screw up this planet enough, they won't want it anymore! You know what? It is beets. I've crashed into a beet truck.",
+            date: "1/2/2021",
+        },
+        tweetTwo = {
+            user: "John",
+            content: "They're using our own satellites against us. And the clock is ticking. God help us, we're in the hands of engineers. You know what? It is beets. I've crashed into a beet truck. What do they got in there? King Kong? You know what? It is beets. I've crashed into a beet truck.",
+            date: "1/14/2021",
+        },
+        tweetThree = {
+            user: "Javale",
+            content: "Jaguar shark! So tell me - does it really exist? Checkmate... God creates dinosaurs. God destroys dinosaurs. God creates Man. Man destroys God. Man creates Dinosaurs. God creates dinosaurs. God destroys dinosaurs. God creates Man. Man destroys God. Man creates Dinosaurs.",
+            date: "6/21/2020",
+        },
+        tweetFour = {
+            user: "Barry",
+            content: "Remind me to thank John for a lovely weekend. I was part of something special. You really think you can fly that thing? Forget the fat lady! You're obsessed with the fat lady! Drive us out of here! Hey, you know how I'm, like, always trying to save the planet? Here's my chance.",
+            date: "3/24/2019",
+        },
+        tweetFive = {
+            user: "Rick",
+            content: "I gave it a cold? I gave it a virus. A computer virus. Just my luck, no ice. Do you have any idea how long it takes those cups to decompose. My dad once told me, laugh and the world laughs with you, Cry, and I'll give you something to cry about you little bastard!",
+            date: "8/11/2018",
+        }
+    ],
+    }
+  },
+};
+</script>
+
+<style scoped>
+</style>
